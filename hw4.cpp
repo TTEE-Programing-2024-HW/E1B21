4e1444abbd5f9f67e27adf7cd1edb04627af96e4
E1B21jerry <u11202221@o365.ttu.edu.tw>
2024-05-25 21:37:05 +0800

    設置會用到的變數以及包含姓名學號數學成績物理成績英文成績和平均成績的結構data

---
+++ hw4.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<unistd.h>
+#include<string.h>
+struct data
+	{
+		char name[12];
+		char num[6];
+		int math;
+		int phy;
+		int eng;
+		float avg;
+	};
+int main(void)
+{
+	struct data *student;
+	char ch,nam[12],cho;
+	int i=0,a=0,b=3,n=0,z=1,w=2;
+}
