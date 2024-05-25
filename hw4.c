f905e5ffbc4821ae55843fa82f8aa316257cf4a1
E1B21jerry <u11202221@o365.ttu.edu.tw>
2024-05-25 21:50:02 +0800

    在螢幕上輸出圖片(可愛的依布)等待使用者案任一件後清除螢幕並讓使用者輸入密碼

--- hw4.c
+++ hw4.c
@@ -17,5 +17,36 @@ int main(void)
 	struct data *student;
 	char ch,nam[12],cho;
 	int i=0,a=0,b=3,n=0,z=1,w=2;
-	
+	puts("    ::                                             ");     
+    puts("   ??~.                                            ");     
+    puts("  :5#57:                                           ");     
+    puts("  ~PBGPJ^                                          ");     
+    puts("  ^5GGGPJ~                                         ");     
+    puts("  .YGGGGPJ^                                        ");     
+    puts("   !PBGGBP?.  ..  ..                ....::^~~:     ");     
+    puts("   .?G#B#&J^^^!!~^!!::.       .:~!?JY55PGG5?^.     ");     
+    puts("    .7P#@@5??777777!77!!:  .~?5GBBGGGGGGPJ~.   ..  ");     
+    puts("      ^?G5??77777!!!777??!7YB&@@#GGGGG5?~.    :?!  ");     
+    puts("        !?7?7?777777????7?Y#@@@&&#BPY7:    .:~~^!^ ");     
+    puts("         7?JJPJ?????????????55JJ?7!~^:::^^^~~~^^^~!");      
+    puts("       .J?&#&???????7Y?P?7??.   ^!7?JJJ??7!77~^^~! ");     
+    puts("       ^??B&G??7???75@B@J?J^  :?YYYJJJYYYYYYJ!^^~~ ");    
+    puts("    .^^???JY??7??777G#&BJJ?. :YYJJJJJJJJJJJJJ!~~!^ ");     
+    puts("   ~~::^?J?????J???7?JYJJY!~^J5YJJJJJJJJJJJJJ?YJ7  ");     
+    puts("  .~..:::~7??J???????JJJ?!~~~?55YYYYYJJJJJYYY55P:  ");     
+    puts("  ^7::::^:::^~!7????77?!~~~~~~555555YYYYYY555P5~   ");     
+    puts("  .!7^:~:.:^:::::^^~!~~!~~~~!?YJ55555555555P5?:    ");     
+    puts("    :~!7::^...:..::^!7~?!~~~JYJJJ555PP555YJ!:      ");     
+    puts("      .~!~!^:::::::~!7!?!!?JJJJJJY!^~~^^:.         ");     
+    puts("        :7??!~~~^^~!7?YYJJJJJJJJJY:                ");     
+    puts("         :YJJ?~!77YJJJJ5Y5J?????JY:                ");     
+    puts("          !JJJJ5YJJJJJ5PPG!.?777??                 ");     
+    puts("           ????J?7???JGPP! :?7???:                 ");     
+    puts("          .7777J?7???YYJ: !J????^                  ");     
+    puts("          :????J777?7..   ~77?7^                   ");      
+    puts("            .:.^777!.                              ");             
+	system("PAUSE"); // 螢幕畫面暫停，並等待使用者按任意鍵。
+	system("CLS"); // 清除螢幕
+	printf("請輸入密碼");
+	scanf("%d",&a);
 }
